1e1b061f27936a8499b28177a6a1c0e3d6d03308
brunomont <brunoadrian.mont@gmail.com>
2022-04-09 11:55:43 -0500

    estado inicial en null

--- src/context/MyContext.js
+++ src/context/MyContext.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export const MyContext = React.createContext({});
 export function MyContextProvider({ children }) {
-  const [result, setResult] = useState([]);
+  const [result, setResult] = useState(null);
   return (
     <MyContext.Provider value={{ result, setResult }}>
       {children}

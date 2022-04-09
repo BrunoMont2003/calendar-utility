2bb8e6a4d27c79edf69dee0c001d19a87f76bde3
brunomont <brunoadrian.mont@gmail.com>
2022-04-09 10:36:29 -0500

    initial step with react-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-// import "flowbite";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(
+  <BrowserRouter>
+    <App tab="home" />
+  </BrowserRouter>
+);
